23d33324fac5ed3d2e6b6cc0eef052596d330f06
Anmol Wadhwa <anmolwadhwa7.sk@gmail.com>
2025-04-10 16:55:23 +0530

    God damn stupid error

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,17 +1,17 @@
+const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("./jwtconfig");
 
 
 const authmiddleware= (req,res,next)=>{
     const authheader = req.headers.authorization;
     if(!authheader || !authheader.startsWith('Bearer ')){
-        return res.status(403).json({})
+        return res.status(403).json({message : "token missing"});
     }
 
     const token = authheader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token,JWT_SECRET);
-        console.log(decoded);
         if(decoded.userId){
             req.userId = decoded.userId;
             next();
@@ -22,7 +22,7 @@ const authmiddleware= (req,res,next)=>{
     } 
     
     catch (err) {
-        return res.status(403).json({})
+        return res.status(403).json({message : "Invalid Token"})
     }
 
 }
