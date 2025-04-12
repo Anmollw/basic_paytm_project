7d1c7b82a8caad9a081d51d2ad3308acb22c3c57
Anmol Wadhwa <anmolwadhwa7.sk@gmail.com>
2025-04-12 23:08:39 +0530

    Added more props to input box component

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,10 +1,10 @@
 
-export function InputBox({label,placeholder}){
+export function InputBox({label,placeholder,OnChange}){
     return <div className="text-sm font-medium text-left py-2">
         <div>
           {label}
         </div>
-        <input placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+        <input onChange={OnChange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
    </div>
 
 }
